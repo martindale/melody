0bbf1db4e3996de526f4bc191af01975502afeb9
Eric Martindale <eric@ericmartindale.com>
2015-09-30 17:39:53 -0700

    Add contact form.

--- soundtrack:blog.js
+++ soundtrack:blog.js
@@ -1,3 +1,18 @@
 var melody = require('./');
+var Remote = require('maki-remote');
 
-melody.start();
+var MailPimpTask = new Remote('http://localhost:2525/tasks');
+
+melody.start(function(err) {
+  melody.app.post('/contact', function(req, res, next) {
+    MailPimpTask.create({
+      subject: 'soundtrack.io Contact Form',
+      recipient: 'eric@decentralize.fm',
+      sender: req.param('from'),
+      content: req.param('message')
+    }, function(err, task) {
+      req.flash('info', 'Mail sent successfully!  We\'ll get in touch shortly.');
+      res.redirect('/');
+    });
+  });
+});
