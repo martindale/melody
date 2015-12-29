35d27d6c7fd310c0b20f6977abbed6940edc8210
Eric Martindale <eric@ericmartindale.com>
2015-12-29 10:30:31 -0500

    Package up and simplify initial setup.

--- config/index.js
+++ config/index.js
@@ -1,16 +1,25 @@
 module.exports = {
   service: {
-    name: 'melodies',
-    synopsis: 'thoughts from soundtrack.io',
-    mission: 'thoughts from the soundtrack.io team.',
-    icon: 'music'
+    name: 'Your Stream Name', // change this!
+    synopsis: 'Thoughts and musings from me.', // and this...
+    mission: 'Self-hosted writing and content sharing, powered by Maki', // also
+    icon: 'write' // pick from: http://semantic-ui.com/elements/icon.html
+  },
+  config: {
+    allowPublicRegistration: false, // let other people register
+    characterLimit: false // set to a number, such as 140.
+  },
+  users: { // users will be created on startup, password logged to console
+    'admin': { // admin username will be 'admin', change if you'd like
+      roles: ['admin'] // what roles this user will have
+    }
   },
   database: {
-    name: 'soundtrack'
+    name: 'melody' // only change if you know what you're doing
   },
   services: {
     http: {
-      port: 13005
+      port: 13005 // you might want this to be 80, or 3000
     }
   }
 };
